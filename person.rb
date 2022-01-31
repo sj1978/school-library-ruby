8830127a47d5f8c227d8160ef21f43aead36904e
Anibal Amoroso <76854002+sj1978@users.noreply.github.com>
2022-01-31 17:24:42 -0300

    add files and classes

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def of_age?
+    @age >= 18
+  end
+
+  # public
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
