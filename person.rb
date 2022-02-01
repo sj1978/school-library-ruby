ed6e3ed4f4ff5c41ece8dd0d3d9c7f87a9f15357
Anibal Amoroso <76854002+sj1978@users.noreply.github.com>
2022-02-01 19:14:31 -0300

    add corrector class

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
+require './corrector'
+
 class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = rand(1000)
     @name = name
     @age = age
+    @corrector = Corrector.new
     @parent_permission = parent_permission
   end
   attr_reader :id
@@ -16,5 +19,9 @@ class Person
 
   def of_age?
     @age >= 18
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
   end
 end
