2a7171a8358de9dd59a5b64a477b753b16d264ea
Anibal Amoroso <76854002+sj1978@users.noreply.github.com>
2022-02-03 00:12:20 -0300

    add requested changes

--- person.rb
+++ person.rb
@@ -18,9 +18,8 @@ class Person
     of_age? || @parent_permission
   end
 
-  def add_rental(rental)
-    @rentals.push(rental)
-    rental.person = self
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private
--- student.rb
+++ student.rb
@@ -2,14 +2,16 @@ require './person'
 require './classroom'
 
 class Student < Person
-  attr_accessor :classroom
-
   def initialize(age, name = 'Unknown', parent_permission = 'unknown')
     super(age, name, parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
